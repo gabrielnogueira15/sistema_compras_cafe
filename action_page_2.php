6d15fc375e887bd4beeec75bcdabc3138dd03061
gabrielnogueira15 <137405758+gabrielnogueira15@users.noreply.github.com>
2023-07-12 12:23:21 -0300

    Adicionando novas funcionalidades
    Adicionando teste de consulta ao banco com echo na página

---
+++ action_page_2.php
@@ -0,0 +1,31 @@
+<?php
+
+/*echo "Olá " . $_POST["bobina"] . " (email: " . $_POST["acucar"] . ")<br><br>";
+echo "Sua mensagem: " . $_POST["guardanapo"];*/
+
+
+$con = mysqli_connect('localhost', 'root', '', 'compras_cafe');
+
+$hoje = date('Y/m/d');
+$sucoMa = $_POST['sucoMa'];
+$guardanapo = $_POST['guardanapo'];
+$bobina = $_POST['bobina'];
+$papelToalha = $_POST['papelToalha'];
+
+$sql = "INSERT INTO compras_favareto (dataHoje, sucoMa, guardanapo, bobina, papelToalha) 
+VALUES ('$hoje', '$sucoMa', '$guardanapo', '$bobina', '$papelToalha')";
+
+$rs = mysqli_query($con, $sql);
+
+if($rs){
+    echo "Dados inseridos";
+}
+else{
+    echo "Erro ao inserir dados";
+}
+
+
+
+
+
+?>
